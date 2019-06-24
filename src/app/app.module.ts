9f432e0f324327156a0d006bde7dc54780dd2d80
Raj Kumar <raj.kumar@scoratech.com>
2019-06-24 23:34:08 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RxjsOperatorComponent } from './rxjs-operator/rxjs-operator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RxjsOperatorComponent
   ],
   imports: [
     BrowserModule,
